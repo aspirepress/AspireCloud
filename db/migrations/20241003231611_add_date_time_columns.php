e7effd624e5666d0a225652fef15673d089037dc
Sarah Savage <me@sarah-savage.com>
2024-10-03 19:21:29 -0400

    Adding datetime columns for plugins

---
+++ db/migrations/20241003231611_add_date_time_columns.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AddDateTimeColumns extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $versions = $this->table('plugin_files');
+        $versions->addColumn('created', 'datetime', ['default' => 'NOW()'])
+            ->addColumn('processed', 'datetime', ['null' => true])
+            ->update();
+    }
+}
