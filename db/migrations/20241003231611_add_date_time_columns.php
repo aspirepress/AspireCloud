ba4036b5667a0fd3da52210c3fc57d8b94c23527
Sarah Savage <me@sarah-savage.com>
2024-10-04 07:24:11 -0400

    Adding processed date for plugin files table

--- db/migrations/20241003231611_add_date_time_columns.php
+++ db/migrations/20241003231611_add_date_time_columns.php
@@ -17,11 +17,32 @@ final class AddDateTimeColumns extends AbstractMigration
      * Remember to call "create()" or "update()" and NOT "save()" when working
      * with the Table class.
      */
-    public function change(): void
+    public function up(): void
     {
         $versions = $this->table('plugin_files');
         $versions->addColumn('created', 'datetime', ['default' => 'NOW()'])
             ->addColumn('processed', 'datetime', ['null' => true])
+            ->update();
+
+        $pdo = $this->getAdapter()->getConnection();
+        $sql = 'SELECT id, metadata FROM plugin_files WHERE metadata IS NOT NULL';
+        $update = 'UPDATE plugin_files SET processed = :processed WHERE id = :id';
+        $updateStmt = $pdo->prepare($update);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        while ($row = $stmt->fetch()) {
+            $metadata = json_decode($row['metadata'], true);
+            if (isset($metadata['finalized'])) {
+                $updateStmt->execute([':processed' => $metadata['finalized'], ':id' => $row['id']]);
+            }
+        }
+    }
+
+    public function down(): void
+    {
+        $versions = $this->table('plugin_files');
+        $versions->removeColumn('created')
+            ->removeColumn('processed')
             ->update();
     }
 }
