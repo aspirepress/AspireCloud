220a41db54bbf616d93299a3d6178cda52cbf9bb
Sarah Savage <me@sarah-savage.com>
2024-10-10 07:45:56 -0400

    Removing files we don't need because we don't have a front end

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-// Add JS mixes here.
-//mix.js("resources/js/app.js", "public/js")
-
-mix.postCss("./assets/source/style.css", "./assets/output/style.css", [
-        require("tailwindcss"),
-    ]);
