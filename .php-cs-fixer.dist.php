b20ac8d6ce2a891d520abf3d07c02cf13852aeb8
Chuck Adams <cja987@gmail.com>
2024-10-19 08:57:40 -0600

    qa: switch from pint to php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder)
+    ->in(__DIR__)
+    ->exclude([
+        'vendor',
+        'storage',
+        'bootstrap',
+    ])
+    ->notPath([
+        // 'dump.php',
+        // 'src/exception_file.php',
+    ]);
+
+return (new PhpCsFixer\Config)
+    ->setRules([
+        '@PER-CS' => true,
+        '@PHP83Migration' => true,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__.'/.cache/.php_cs.cache');
