a60c27418d2adff45a0fbfdc812bc616e94c9e02
Chuck Adams <cja987@gmail.com>
2024-10-19 11:20:37 -0600

    chore: note in web.php to use api.php instead

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\CatchAllController;
+// NOTE FOR ASPIRECLOUD: API routes should go in api.php, not here
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
