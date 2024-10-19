619449327f60008dfd1537bb4f302ba5963d102f
Chuck Adams <cja987@gmail.com>
2024-10-18 20:48:16 -0600

    feat: add stub api routes file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
