6400fe85726db09b18746e0a8c712fd325d7cf86
Sarah Savage <me@sarah-savage.com>
2024-10-19 07:08:52 -0400

    Fixing a style issue detected

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
-# Note: api routes are not prefixed, i.e. all routes in here are from the root like web routes
+// Note: api routes are not prefixed, i.e. all routes in here are from the root like web routes
 
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')
     ->group(function (Router $r) {
-        Route::get('/hello', fn() => ['message' => 'hello world']);
+        Route::get('/hello', fn () => ['message' => 'hello world']);
     });
 
-Route::fallback(fn() => ['message' => 'Not Found']);
+Route::fallback(fn () => ['message' => 'Not Found']);
