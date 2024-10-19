6d6e0a55ded691e08a1a2adae461d1159d04abdd
Chuck Adams <cja987@gmail.com>
2024-10-19 11:16:49 -0600

    chore: move version ranges to each endpoint

--- routes/api.php
+++ routes/api.php
@@ -10,37 +10,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/')
     ->group(function (Router $r) {
-        $r->get('/secret-key/{version}', CatchAllController::class);
-        $r->get('/secret-key/{version}/salt', CatchAllController::class);
+        $r->get('/secret-key/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
+        $r->get('/secret-key/{version}/salt', CatchAllController::class)->where(['version' => '1.1']);
 
-        $r->get('/stats/wordpress/{version}', CatchAllController::class);
-        $r->get('/stats/php/{version}', CatchAllController::class);
-        $r->get('/stats/mysql/{version}', CatchAllController::class);
-        $r->get('/stats/locale/{version}', CatchAllController::class);
-        $r->get('/stats/plugin/{version}/downloads.php', CatchAllController::class);
-        $r->get('/stats/plugin/{version}/{slug}', CatchAllController::class);
+        $r->get('/stats/wordpress/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/stats/php/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/stats/mysql/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/stats/locale/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/stats/plugin/{version}/downloads.php', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/stats/plugin/{version}/{slug}', CatchAllController::class)->where(['version' => '1.0']);
 
-        $r->get('/core/browse-happy/{version}', CatchAllController::class);
-        $r->get('/core/checksums/{version}', CatchAllController::class);
-        $r->get('/core/credits/{version}', CatchAllController::class);
-        $r->get('/core/handbook/{version}', CatchAllController::class);
-        $r->get('/core/importers/{version}', CatchAllController::class);
-        $r->get('/core/serve-happy/{version}', CatchAllController::class);
-        $r->get('/core/stable-check/{version}', CatchAllController::class);
-        $r->get('/core/version-check/{version}', CatchAllController::class);
+        $r->get('/core/browse-happy/{version}', CatchAllController::class)->where(['version' => '1.1']);
+        $r->get('/core/checksums/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/core/credits/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
+        $r->get('/core/handbook/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/core/importers/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
+        $r->get('/core/serve-happy/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/core/stable-check/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/core/version-check/{version}', CatchAllController::class)->where(['version' => '1.[67]']);
 
-        $r->get('/translations/core/{version}', CatchAllController::class);
-        $r->get('/translations/plugins/{version}', CatchAllController::class);
-        $r->get('/translations/themes/{version}', CatchAllController::class);
+        $r->get('/translations/core/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/translations/plugins/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/translations/themes/{version}', CatchAllController::class)->where(['version' => '1.0']);
 
-        $r->get('/themes/info/{version}', CatchAllController::class);
-        $r->get('/themes/update-check/{version}', CatchAllController::class);
+        $r->get('/themes/info/{version}', CatchAllController::class)->where(['version' => '1.[012]']);
+        $r->get('/themes/update-check/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
 
-        $r->get('/plugins/info/{version}', CatchAllController::class);
-        $r->get('/plugins/update-check/{version}', CatchAllController::class);
+        $r->get('/plugins/info/{version}', CatchAllController::class)->where(['version' => '1.[012]']);
+        $r->get('/plugins/update-check/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
 
-        $r->get('/patterns/{version}', CatchAllController::class);
-        $r->get('/events/{version}', CatchAllController::class);
-    })->where(['version' => '[0-9]+\.[0-9]+']);
+        $r->get('/patterns/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $r->get('/events/{version}', CatchAllController::class)->where(['version' => '1.0']);
+    });
 
-Route::any('{path}', CatchAllController::class)->where('path', '.*');
+// Route::any('{path}', CatchAllController::class)->where('path', '.*');
