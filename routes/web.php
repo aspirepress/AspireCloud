94344bad7719ab6e78c90f1cccdc518f962f6ad5
Chuck Adams <cja987@gmail.com>
2024-10-23 15:03:23 -0600

    fix: scrape laravel off, saving some pieces in .ATTIC

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// NOTE FOR ASPIRECLOUD: API routes should go in api.php, not here
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
