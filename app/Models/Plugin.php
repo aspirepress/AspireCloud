2b998f4e77f4e11c7e25d310ede488f6eb8e68fe
Chuck Adams <cja987@gmail.com>
2024-10-22 23:37:57 -0600

    feat: WIP Plugin and Theme models

---
+++ app/Models/Plugin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Plugin extends BaseModel {
+    use HasUuids;
+
+    protected $table = 'plugins';
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'slug' => 'string',
+            'name' => 'string',
+            'short_description' => 'string',
+            'description' => 'string',
+            'version' => 'string',
+            'author' => 'string',
+            'requires' => 'string',
+            'requires_php' => 'string',
+            'tested' => 'string',
+            'download_link' => 'string',
+            'added' => 'datetime',
+            'last_updated' => 'datetime',
+            'author_profile' => 'string',
+            'rating' => 'integer',
+            'ratings' => 'array',
+            'num_ratings' => 'integer',
+            'support_threads' => 'integer',
+            'support_threads_resolved' => 'integer',
+            'active_installs' => 'integer',
+            'downloaded' => 'integer',
+            'homepage' => 'string',
+            'banners' => 'array',
+            'tags' => 'array',
+            'donate_link' => 'string',
+            'contributors' => 'array',
+            'icons' => 'array',
+            'source' => 'array',
+            'business_model' => 'string',
+            'commercial_support_url' => 'string',
+            'support_url' => 'string',
+            'preview_link' => 'string',
+            'repository_url' => 'string',
+            'requires_plugins' => 'array',
+            'compatibility' => 'array',
+            'screenshots' => 'array',
+            'sections' => 'array',
+            'versions' => 'array',
+            'upgrade_notice' => 'array',
+        ];
+    }
+}
---
+++ app/Models/Theme.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Theme extends BaseModel {
+    use HasUuids;
+
+    protected $table = 'themes';
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'slug' => 'string',
+            'name' => 'string',
+            'version' => 'string',
+            'download_link' => 'string',
+            'requires_php' => 'string',
+            'last_updated' => 'datetime',
+            'creation_time' => 'datetime',
+            'preview_url' => 'string',
+            'screenshot_url' => 'string',
+            'ratings' => 'array',
+            'rating' => 'integer',
+            'num_ratings' => 'integer',
+            'reviews_url' => 'string',
+            'downloaded' => 'integer',
+            'active_installs' => 'integer',
+            'homepage' => 'string',
+            'sections' => 'array',
+            'tags' => 'array',
+            'versions' => 'array',
+            'requires' => 'array',
+            'is_commercial' => 'boolean',
+            'external_support_url' => 'string',
+            'is_community' => 'boolean',
+            'external_repository_url' => 'string',
+        ];
+    }
+}
