f8a673c71ffa6f41c53d4c5f93e4e7444e46a99b
Chuck Adams <cja987@gmail.com>
2024-10-23 16:35:32 -0600

    feat: add doctrine/doctrine-migrations-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
