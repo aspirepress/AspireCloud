952e7437891866a040e94a42f3ec5f1aac4d6173
Chuck Adams <cja987@gmail.com>
2024-10-23 20:19:50 -0600

    chore: add remaining sync tables

--- migrations/Version20241023234555.php
+++ migrations/Version20241023234555.php
@@ -38,13 +38,44 @@ final class Version20241023234555 extends AbstractMigration
               version varchar(32) not null,
               metadata jsonb default null,
               created timestamp without time zone default current_timestamp,
-              processed_at timestamp without time zone default null,
+              processed timestamp without time zone default null,
               hash varchar(255) not null,
-
-              primary key(id),
+              primary key (id),
               constraint fk_sync_plugin_id
                   foreign key(plugin_id)
                   references sync_plugins(id)
+                  on delete cascade
+          )
+      SQL;
+
+    const string ADD_SYNC_THEMES_PGSQL = <<<'SQL'
+        create table sync_themes (
+            id uuid not null,
+            name varchar(1024) not null,
+            slug varchar(255) not null,
+            current_version varchar(32) default null,
+            updated_at timestamp without time zone default current_timestamp,
+            pulled_at timestamp without time zone default null,
+            metadata jsonb default null,
+            primary key (id)
+        )
+    SQL;
+
+    const string ADD_SYNC_THEME_FILES_PGSQL = <<<'SQL'
+          create table sync_theme_files (
+              id uuid not null,
+              theme_id uuid not null,
+              file_url varchar(1024) default null,
+              type varchar(32) not null,
+              version varchar(32) not null,
+              metadata jsonb default null,
+              created timestamp without time zone default current_timestamp,
+              processed timestamp without time zone default null,
+              hash varchar(255) not null,
+              primary key (id),
+              constraint fk_sync_theme_id
+                  foreign key(theme_id)
+                  references sync_themes(id)
                   on delete cascade
           )
       SQL;
@@ -54,10 +85,21 @@ final class Version20241023234555 extends AbstractMigration
             id uuid not null,
             command varchar(255) not null,
             stats jsonb not null,
-            created_at timestamp without time zone default current_timestamp
+            created_at timestamp without time zone default current_timestamp,
+            primary key (id)
         )
     SQL;
 
+    // TODO: give this the sync_ prefix treatment (needs a PR in AspireSync)
+    const string ADD_SYNC_NOT_FOUND_ITEMS_PGSQL = <<<'SQL'
+        create table not_found_items (
+            id uuid not null,
+            item_type varchar(32) not null,
+            item_slug varchar(255) not null,
+            created_at timestamp without time zone default current_timestamp,
+            updated_at timestamp without time zone default current_timestamp
+        )
+    SQL;
 
     const string ADD_INDEXES_PGSQL = <<<'SQL'
         create index idx_sync_plugins_name on sync_plugins(name);
@@ -68,6 +110,9 @@ final class Version20241023234555 extends AbstractMigration
         drop table if exists sync_stats;
         drop table if exists sync_plugin_files;
         drop table if exists sync_plugins;
+        drop table if exists sync_theme_files;
+        drop table if exists sync_themes;
+        drop table if exists not_found_items;
     SQL;
 
     public function up(Schema $schema): void
@@ -75,7 +120,10 @@ final class Version20241023234555 extends AbstractMigration
         foreach ([
                      self::ADD_SYNC_PLUGINS_PGSQL,
                      self::ADD_SYNC_PLUGIN_FILES_PGSQL,
+                     self::ADD_SYNC_THEMES_PGSQL,
+                     self::ADD_SYNC_THEME_FILES_PGSQL,
                      self::ADD_SYNC_STATS_PGSQL,
+                     self::ADD_SYNC_NOT_FOUND_ITEMS_PGSQL,
                      self::ADD_INDEXES_PGSQL,
                  ] as $section) {
             foreach (preg_split('/;\n/', $section) as $statement) {
