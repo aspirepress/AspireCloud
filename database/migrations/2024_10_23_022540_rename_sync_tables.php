0e7619ab0e8aaa86db89291f46290868a39abe3b
Chuck Adams <cja987@gmail.com>
2024-10-26 09:41:53 -0600

    fix: not_found_items -> sync_not_found_items

--- database/migrations/2024_10_23_022540_rename_sync_tables.php
+++ database/migrations/2024_10_23_022540_rename_sync_tables.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::rename('stats', 'sync_stats');
         Schema::rename('theme_files', 'sync_theme_files');
         Schema::rename('themes', 'sync_themes');
+        Schema::rename('not_found_items', 'sync_not_found_items');
     }
 
     public function down(): void
@@ -24,5 +25,6 @@ return new class extends Migration {
         Schema::rename('sync_stats', 'stats');
         Schema::rename('sync_theme_files', 'theme_files');
         Schema::rename('sync_themes', 'themes');
+        Schema::rename('sync_not_found_items', 'not_found_items');
     }
 };
