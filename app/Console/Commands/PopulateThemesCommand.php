4b5e9a509d08b842b89a40b35fdb70b4bb29f155
Chuck Adams <cja987@gmail.com>
2024-10-27 15:15:11 -0600

    feat: new command: db:themes:populate

---
+++ app/Console/Commands/PopulateThemesCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Models\Sync\SyncTheme;
+use App\Models\WpOrg\Theme;
+
+class PopulateThemesCommand extends Command
+{
+    protected $signature = 'db:themes:populate';
+
+    protected $description = 'Loads themes from AspireSync metadata';
+
+    public function handle(): void
+    {
+        SyncTheme::cursor()->each(function (SyncTheme $sync) {
+           $this->info("$sync->slug ...");
+           Theme::getOrCreateFromSyncTheme($sync);
+        });
+    }
+}
