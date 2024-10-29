f82d0db5a2a283fc903709b17531b49b8e9178f4
Chuck Adams <cja987@gmail.com>
2024-10-29 08:00:33 -0600

    chore: fill out populate commands with more options

--- app/Console/Commands/PopulatePluginsCommand.php
+++ app/Console/Commands/PopulatePluginsCommand.php
@@ -5,21 +5,34 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\Sync\SyncPlugin;
 use App\Models\WpOrg\Plugin;
+use Illuminate\Support\Facades\DB;
 
 class PopulatePluginsCommand extends Command
 {
-    protected $signature = 'db:populate:plugins';
+    protected $signature = 'db:populate:plugins {--create-only} {--days=}';
 
     protected $description = 'Loads plugins from AspireSync metadata';
 
     public function handle(): void
     {
-        SyncPlugin::query()
-            ->where('status', 'open')
-            ->cursor()
-            ->each(function (SyncPlugin $sync) {
-                $this->info("$sync->slug ...");
-                Plugin::getOrCreateFromSyncPlugin($sync);
-            });
+        $create_only = $this->option('create-only');
+        $days = (int)$this->option('days');
+
+        if ($this->option('delete')) {
+            $query = DB::table('themes')->whereNotNull('sync_id');
+            $days and $query->whereDate('pulled_at', '>=', now()->subDays($days));
+            $query->delete();
+        }
+
+        $query = SyncPlugin::query()->where('status', 'open');
+        $days and $query->whereDate('pulled_at', '>=', now()->subDays($days));
+
+        foreach ($query->cursor() as $sync) {
+            $this->info("$sync->slug ...");
+            $plugin = Plugin::getOrCreateFromSyncPlugin($sync);
+            if (!$create_only) {
+                $plugin->updateFromSyncPlugin();
+            }
+        }
     }
 }
--- app/Console/Commands/PopulateThemesCommand.php
+++ app/Console/Commands/PopulateThemesCommand.php
@@ -2,21 +2,37 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\Sync\SyncTheme;
 use App\Models\WpOrg\Theme;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class PopulateThemesCommand extends Command
 {
-    protected $signature = 'db:populate:themes';
+    protected $signature = 'db:populate:themes {--days=} {--create-only} {--delete}';
 
     protected $description = 'Loads themes from AspireSync metadata';
 
     public function handle(): void
     {
-        SyncTheme::cursor()->each(function (SyncTheme $sync) {
+        $create_only = $this->option('create-only');
+        $days = (int)$this->option('days');
+
+        if ($this->option('delete')) {
+            $query = DB::table('themes')->whereNotNull('sync_id');
+            $days and $query->whereDate('pulled_at', '>=', now()->subDays($days));
+            $query->delete();
+        }
+
+        $query = SyncTheme::query();
+        $days and $query->whereDate('pulled_at', '>=', now()->subDays($days));
+
+        foreach ($query->cursor() as $sync) {
             $this->info("$sync->slug ...");
-            Theme::getOrCreateFromSyncTheme($sync);
-        });
+            $theme = Theme::getOrCreateFromSyncTheme($sync);
+            if (!$create_only) {
+                $theme->updateFromSyncTheme();
+            }
+        }
     }
 }
