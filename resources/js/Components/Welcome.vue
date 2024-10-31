a9cb97c28f31312cd690463165f65471239e91ea
Enrique Chavez <noone@tmeister.net>
2024-10-30 20:20:24 -0600

    feat: Add all required files and migrations to setup Jetstream

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+</script>
+
+<template>
+    <div class="p-3">
+        Temporal placeholder
+    </div>
+</template>
