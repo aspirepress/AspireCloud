bb9b6e4c5229071533f20bb8d4d3d7d84570822c
Enrique Chavez <noone@tmeister.net>
2024-10-31 21:19:17 -0600

    fix: remove duplicate array

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,17 +16,6 @@ $apiPaths = [
     'events/*',
 ];
 
-$apiPaths = [
-    'secret-key/*',
-    'stats/*',
-    'core/*',
-    'translations/*',
-    'themes/*',
-    'plugins/*',
-    'patterns/*',
-    'events/*',
-];
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
