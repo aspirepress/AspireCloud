1b28dc305f5de25037b4a59fc77d12aa465b507b
Chuck Adams <cja987@gmail.com>
2024-10-31 23:34:07 -0600

    refactor: break when clauses out into methods in QueryThemesService

--- app/Services/Themes/QueryThemesService.php
+++ app/Services/Themes/QueryThemesService.php
@@ -18,38 +18,62 @@ class QueryThemesService
 
         $themesBaseQuery = Theme::query()
             ->orderBy('last_updated', 'desc')   // default sort
-            ->when($req->browse, function ($query, $browse) {
-                // TODO: replicate 'featured' browse (currently it's identical to 'popular')
-                match ($browse) {
-                    'popular', 'featured' => $query->reorder('rating', 'desc'),
-                    'new' => $query->reorder('creation_time', 'desc'),
-                    default => null,
-                };
-            })
-            ->when($req->search, function ($query, $search) {
-                $query->where('name', 'ilike', "%{$search}%")
-                    ->orWhereFullText('description', $search);
-            })->when($req->theme, function ($query, $search) {
-                $query->where('slug', 'ilike', $search);
-            })->when($req->author, function (Builder $query, string $author) {
-                $query->whereHas('author', function (Builder $query) use ($author) {
-                    $query->where('user_nicename', 'like', "%{$author}%");
-                });
-            })->when($req->tags, function (Builder $query, array $tags) {
-                $query->whereHas('tags', function (Builder $tagQuery) use ($tags) {
-                    $tagQuery->whereIn('slug', $tags);
-                });
-            });
+            ->when($req->browse, self::applyBrowse(...))
+            ->when($req->search, self::applySearch(...))
+            ->when($req->theme, self::applyTheme(...))
+            ->when($req->author, self::applyAuthor(...))
+            ->when($req->tags, self::applyTags(...));
+
+        $total = $themesBaseQuery->count();
+
         $themes = $themesBaseQuery
             ->skip($skip)
             ->take($perPage)
             ->with('author')
             ->get();
-        $total = $themesBaseQuery->count();
 
-        $collection = collect($themes)->map(fn($theme,
-        ) => (new ThemeResource($theme))->additional(['fields' => $req->fields]));
+        $collection = collect($themes)
+            ->map(fn($theme) => (new ThemeResource($theme))->additional(['fields' => $req->fields]));
 
         return new ThemeCollection($collection, $page, (int) ceil($total / $perPage), $total);
+    }
+
+    /** @param Builder<Theme> $query */
+    private static function applyBrowse(Builder $query, string $browse): void
+    {
+        // TODO: replicate 'featured' browse (currently it's identical to 'popular')
+        match ($browse) {
+            'popular', 'featured' => $query->reorder('rating', 'desc'),
+            'new' => $query->reorder('creation_time', 'desc'),
+            default => null,
+        };
+    }
+
+    /** @param Builder<Theme> $query */
+    private static function applySearch(Builder $query, string $search): void
+    {
+        $query->where('name', 'like', "%$search%")
+            ->orWhereFullText('description', $search);
+    }
+
+    /** @param Builder<Theme> $query */
+    private static function applyTheme(Builder $query, string $theme): void
+    {
+        $query->where('slug', 'like', "%$theme%");
+    }
+
+    /** @param Builder<Theme> $query */
+    private static function applyAuthor(Builder $query, string $author): void
+    {
+        $query->whereHas('author', fn(Builder $q) => $q->where('user_nicename', 'like', "%$author%"));
+    }
+
+    /**
+     * @param Builder<Theme> $query
+     * @param string[] $tags
+     */
+    private static function applyTags(Builder $query, array $tags): void
+    {
+        $query->whereHas('tags', fn(Builder $q) => $q->whereIn('slug', $tags));
     }
 }
