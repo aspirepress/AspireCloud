20139651c9ebf427d66868418277f7ca1e29a606
Chuck Adams <cja987@gmail.com>
2024-11-01 20:01:41 -0600

    fix: add TrustProxies middleware with $proxies = "*"

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class TrustProxies extends \Illuminate\Http\Middleware\TrustProxies {
+    protected $proxies = "*";
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 $apiPaths = [
     'secret-key/*',
@@ -18,14 +26,29 @@ $apiPaths = [
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         apiPrefix: '',
     )
-    ->withMiddleware(function () {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // https://laravel.com/docs/11.x/middleware#manually-managing-laravels-default-middleware-groups
+        $middleware->group('web', [
+            TrustProxies::class, // overridden with $proxies = "*"
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            ValidateCsrfToken::class,
+            SubstituteBindings::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+        ]);
+        $middleware->group('api', [
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // 'throttle:api',
+            SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) use ($apiPaths) {
         /**
@@ -39,10 +62,8 @@ return Application::configure(basePath: dirname(__DIR__))
          * on the api endpoints
          * */
         $exceptions->render(function (AuthenticationException $e, Request $request) use ($apiPaths) {
-            if ($request->expectsJson() || collect($apiPaths)->contains(fn ($path) => $request->is($path))) {
-                return response()->json([
-                    'message' => 'Unauthenticated.',
-                ], 401);
+            if ($request->expectsJson() || collect($apiPaths)->contains(fn($path) => $request->is($path))) {
+                return response()->json(['message' => 'Unauthenticated.'], 401);
             }
         });
     })
