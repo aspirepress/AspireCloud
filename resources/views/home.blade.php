0eef7734a9eb28477e7dc363209b5cb06575417c
Chuck Adams <cja987@gmail.com>
2025-01-21 19:23:00 -0700

    AC-131: placeholder homepage to replace default laravel page (#136)
    * feat: dazzling new homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,102 @@
+<?php
+
+use App\Models\User;
+
+/** @var User $user */
+$user ??= auth()->user();
+?>
+@extends('layouts.plain')
+
+@section('title', 'Welcome - AspireCloud')
+
+@section('content')
+    <main class="homepage">
+        <div class="message">
+            <p>
+                Welcome to the AspireCloud API, a service of <a href="https://www.aspirepress.org">AspirePress</a>.
+            </p>
+            <p>
+                Right now, we're currently in beta testing, but anyone can join.
+                During the beta, you'll need an auth token to access the API: you can get one by registering using
+                the link below, then selecting "API Tokens" from the top-right menu.
+            </p>
+            <hr>
+            <p>Useful links:
+                <a href="https://github.com/aspirepress/aspirecloud" target="_blank">Github</a>
+                | <a href="https://codex.wordpress.org/WordPress.org_API" target="_blank">WordPress API Reference</a>
+            </p>
+        </div>
+
+        <nav>
+            <ul>
+                @if ($user)
+                    <li><a href="{{route('dashboard')}}">Dashboard</a></li>
+                    <li>
+                        <a href="{{ route('logout') }}"
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                        >
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
+                @else
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @endif
+            </ul>
+        </nav>
+    </main>
+@endsection
+
+@section('head')
+    <style>
+        main.homepage {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 66vh;
+
+            font-family: sans-serif;
+            font-size: 1.5rem;
+        }
+
+        main.homepage .message {
+            width: 66%;
+
+            border: 1px solid lightgrey;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            padding: 1em;
+            border-radius: 8px;
+            background-color: white;
+        }
+
+        main.homepage nav {
+            width: 66%;
+            font-size: 3rem;
+        }
+
+        main.homepage nav ul {
+            list-style: none;
+            display: flex;
+            flex-direction: row;
+            padding: 0;
+            justify-content: space-evenly;
+        }
+
+        main.homepage nav li {
+            margin-top: 1em;
+        }
+
+        main.homepage a {
+            text-decoration: none;
+            color: darkcyan;
+        }
+
+        main.homepage a:hover {
+            color: deepskyblue;
+        }
+    </style>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::view('/', 'home');
 
 Route::middleware([
     'auth:sanctum',
