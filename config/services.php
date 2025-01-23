c79e1cd8f992d3ebafe4c5ba0ca1e1a764cba7cd
Chuck Adams <cja987@gmail.com>
2025-01-23 06:59:55 -0700

    style: death to Captain Obvious

--- config/services.php
+++ config/services.php
@@ -1,18 +1,6 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
