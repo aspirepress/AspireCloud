b815ae2cba3528cc7c6d53515b20dd533329fc08
Chuck Adams <cja987@gmail.com>
2025-01-30 11:11:45 -0700

    wip: consolidate remaining migrations

--- database/migrations/2025_01_01_000100_aspirecloud.php
+++ database/migrations/2025_01_01_000100_aspirecloud.php
@@ -11,83 +11,121 @@ return new class extends Migration {
             $table->uuid('id')->primary();
             $table->string('slug')->index();
             $table->string('name')->index();
-            $table->string('short_description', 150)->fulltext();
+            $table->string('short_description', 150);
             $table->text('description');
-            $table->string('version');
+            $table->string('version')->index();
             $table->string('author')->index();
             $table->string('requires');
-            $table->string('requires_php');
+            $table->string('requires_php')->nullable();
             $table->string('tested');
-            $table->string('download_link');
+            $table->string('download_link', 1024);
             $table->dateTime('added')->index();
-            $table->dateTime('last_updated')->index();
-            // all fields below are nullable or have a zero default
+            $table->dateTime('last_updated')->nullable()->index();
             $table->string('author_profile')->nullable();
-            $table->unsignedSmallInteger('rating')->default(0);
+            $table->unsignedSmallInteger('rating')->default(0)->index();
             $table->jsonb('ratings')->nullable();
-            $table->unsignedInteger('num_ratings')->default(0);
-            $table->unsignedInteger('support_threads')->default(0);
-            $table->unsignedInteger('support_threads_resolved')->default(0);
-            $table->unsignedInteger('active_installs')->default(0);
-            $table->unsignedInteger('downloaded')->default(0);
-            $table->string('homepage')->nullable();
+            $table->unsignedInteger('num_ratings')->default(0)->index();
+            $table->unsignedInteger('support_threads')->default(0)->index();
+            $table->unsignedInteger('support_threads_resolved')->default(0)->index();
+            $table->unsignedInteger('active_installs')->default(0)->index();
+            $table->unsignedInteger('downloaded')->default(0)->index();
+            $table->string('homepage', 1024)->nullable();
             $table->jsonb('banners')->nullable();
             $table->jsonb('tags')->nullable();   // denormalized, will be in a join table too
-            $table->string('donate_link')->nullable();
+            $table->string('donate_link', 1024)->nullable();
             $table->jsonb('contributors')->nullable();
             $table->jsonb('icons')->nullable();
             $table->jsonb('source')->nullable();
             $table->string('business_model')->nullable(
             ); // 'commercial'|'community'|false (we'll store false as a string)
-            $table->string('commercial_support_url')->nullable();
-            $table->string('support_url')->nullable();
-            $table->string('preview_link')->nullable();
-            $table->string('repository_url')->nullable();
+            $table->string('commercial_support_url', 1024)->nullable();
+            $table->string('support_url', 1024)->nullable();
+            $table->string('preview_link', 1024)->nullable();
+            $table->string('repository_url', 1024)->nullable();
             $table->jsonb('requires_plugins')->nullable();   // string[]
             $table->jsonb('compatibility')->nullable();      // string[]
             $table->jsonb('screenshots')->nullable();
             $table->jsonb('sections')->nullable();
             $table->jsonb('versions')->nullable();
             $table->jsonb('upgrade_notice')->nullable();
+            $table->string('ac_origin')->nullable()->index();
+            $table->dateTime('ac_created')->useCurrent()->index();
+            $table->jsonb('ac_raw_metadata')->nullable();
+
+            // additional indexes
+            $table->string('slug')->fulltext()->change();
+            $table->string('name')->fulltext()->change();
+            $table->string('short_description')->fulltext()->change();
+            $table->text('description')->fulltext()->change();
+        });
+
+        Schema::create('closed_plugins', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('slug')->index();
+            $table->string('name')->index();
+            $table->text('description');
+            $table->dateTime('closed_date')->index();
+            $table->string('reason')->index();
+            $table->foreignUuid('ac_shadow_id')->nullable()->references('id')->on('plugins')->nullOnDelete();
+            $table->string('ac_origin')->nullable()->index();
+            $table->dateTime('ac_created')->index();
+            $table->jsonb('ac_raw_metadata')->nullable();
+
+            // additional indexes
+            $table->string('slug')->fulltext()->change();
+            $table->string('name')->fulltext()->change();
+            $table->string('description')->fulltext()->change();
         });
 
         Schema::create('authors', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('user_nicename');    // more or less the user "slug", used in permalinks
+            $table->string('user_nicename')->index();
             $table->string('profile')->nullable();
             $table->string('avatar')->nullable();
-            $table->string('display_name')->nullable();
-            $table->string('author')->nullable();
+            $table->string('display_name')->nullable()->index();
+            $table->string('author')->nullable()->index();
             $table->string('author_url')->nullable();
+
+            // additional indexes
+            $table->string('user_nicename')->fulltext()->change();
+            $table->string('display_name')->fulltext()->change();
         });
 
         Schema::create('themes', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('slug');
-            $table->string('name');
-            $table->string('version');
+            $table->string('slug')->index();
+            $table->string('name')->index();
+            $table->text('description')->nullable();
+            $table->string('version')->index();
             $table->foreignUuid('author_id')->references('id')->on('authors');
             $table->string('download_link');
-            $table->string('requires_php');
-            $table->datetime('last_updated');
-            $table->datetime('creation_time');
+            $table->string('requires_php')->nullable();
+            $table->datetime('last_updated')->index();
+            $table->datetime('creation_time')->index();
             $table->string('preview_url')->nullable();
             $table->string('screenshot_url')->nullable();
             $table->jsonb('ratings')->nullable();
-            $table->unsignedSmallInteger('rating')->default(0);
-            $table->unsignedInteger('num_ratings')->default(0);
+            $table->unsignedSmallInteger('rating')->default(0)->index();
+            $table->unsignedInteger('num_ratings')->default(0)->index();
             $table->string('reviews_url')->nullable();
-            $table->unsignedInteger('downloaded')->default(0);
-            $table->unsignedInteger('active_installs')->default(0);
+            $table->unsignedInteger('downloaded')->default(0)->index();
+            $table->unsignedInteger('active_installs')->default(0)->index();
             $table->string('homepage')->nullable();
             $table->jsonb('sections')->nullable();
-            $table->jsonb('tags')->nullable();
             $table->jsonb('versions')->nullable();
             $table->jsonb('requires')->nullable();
             $table->boolean('is_commercial')->default(false);
             $table->string('external_support_url')->nullable();
             $table->boolean('is_community')->default(false);
             $table->string('external_repository_url')->nullable();
+            $table->string('ac_origin')->nullable()->index();
+            $table->datetime('ac_created')->nullable()->index();
+            $table->jsonb('ac_raw_metadata')->nullable();
+
+            // additional indexes
+            $table->string('slug')->fulltext()->change();
+            $table->string('name')->fulltext()->change();
+            $table->string('description')->fulltext()->change();
         });
 
         Schema::create('plugin_tags', function (Blueprint $table) {
@@ -103,15 +141,29 @@ return new class extends Migration {
         });
 
         Schema::create('theme_theme_tags', function (Blueprint $table) {
-            $table->foreignUuid('theme_id')->references('id')->on('themes');
-            $table->foreignUuid('theme_tag_id')->references('id')->on('theme_tags');
+            $table->foreignUuid('theme_id')->references('id')->on('themes')->onDelete('cascade');
+            $table->foreignUuid('theme_tag_id')->references('id')->on('theme_tags')->onDelete('cascade');
             $table->primary(['theme_id', 'theme_tag_id']);  // Composite primary key
         });
 
         Schema::create('plugin_plugin_tags', function (Blueprint $table) {
-            $table->foreignUuid('plugin_id')->references('id')->on('plugins');
-            $table->foreignUuid('plugin_tag_id')->references('id')->on('plugin_tags');
+            $table->foreignUuid('plugin_id')->references('id')->on('plugins')->onDelete('cascade');
+            $table->foreignUuid('plugin_tag_id')->references('id')->on('plugin_tags')->onDelete('cascade');
             $table->primary(['plugin_id', 'plugin_tag_id']);  // Composite primary key
+        });
+
+        Schema::create('assets', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('asset_type');
+            $table->string('slug');
+            $table->string('version')->nullable();
+            $table->string('revision')->nullable();
+            $table->text('upstream_path');
+            $table->text('local_path');
+            $table->string('repository')->default('wp_org')->index();
+            $table->timestamps();
+
+            $table->index(['asset_type', 'slug']);
         });
 
         Schema::create('request_data', function (Blueprint $table) {
@@ -130,12 +182,14 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::dropIfExists('request_data');
+        Schema::dropIfExists('assets');
         Schema::dropIfExists('plugin_plugin_tags');
         Schema::dropIfExists('theme_theme_tags');
         Schema::dropIfExists('theme_tags');
         Schema::dropIfExists('plugin_tags');
         Schema::dropIfExists('themes');
         Schema::dropIfExists('authors');
+        Schema::dropIfExists('closed_plugins');
         Schema::dropIfExists('plugins');
     }
 };
