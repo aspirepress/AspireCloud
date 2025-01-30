bc4db1e341ae20e113090917085f904ffbfcb5b8
Chuck Adams <cja987@gmail.com>
2025-01-30 09:56:03 -0700

    wip: add jetstream fields to use

--- database/migrations/0001_01_01_000100_users.php
+++ database/migrations/0001_01_01_000100_users.php
@@ -14,10 +14,18 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
+
+            // Fortify
             $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
             $table->timestamp('two_factor_confirmed_at')->nullable();
-            $table->rememberToken();
+
+            // Jetstream cruft, apparently required according to the migration name
+            $table->foreignId('current_team_id')->nullable(); // not actually a fk to anything
+            $table->string('profile_photo_path', 2048)->nullable();
+
+            // Last things last
             $table->timestamps();
         });
 
