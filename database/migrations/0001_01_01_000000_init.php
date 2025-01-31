1844ca4e85bb44d331b62fd52021287be1471280
Chuck Adams <cja987@gmail.com>
2025-01-30 21:12:34 -0700

    tweak: enforce use of postgres, our only supported db

--- database/migrations/0001_01_01_000000_init.php
+++ database/migrations/0001_01_01_000000_init.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+        if (Schema::getConnection()->getDriverName() !== 'pgsql') {
+            throw new RuntimeException('AspireCloud only supports running on Postgres');
+        }
+
         if (Schema::hasTable('migrations') && DB::table('migrations')->select('id')->exists()) {
             throw new RuntimeException('Existing migrations detected -- aborting');
         }
