2b62198727c7b8ea360e4076a0c3541cdb6bbc31
Chuck Adams <cja987@gmail.com>
2025-02-06 13:09:05 -0700

    fix: add return type to AuthOptional::handle

--- app/Http/Middleware/AuthOptional.php
+++ app/Http/Middleware/AuthOptional.php
@@ -6,11 +6,12 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class AuthOptional
 {
-    public function handle(Request $request, Closure $next, string $gate)
+    public function handle(Request $request, Closure $next, string $gate): Response
     {
         if ($request->bearerToken()) {
             $user = auth($gate)->user() or throw new UnauthorizedHttpException('Invalid authentication token');
