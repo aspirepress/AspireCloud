c50d210625fccf5b22d86fc979f253709530916a
Chuck Adams <cja987@gmail.com>
2025-02-22 10:03:57 -0700

    tweak: update and reorganize routes

--- routes/api.php
+++ routes/api.php
@@ -24,44 +24,49 @@ Route::prefix('/')
         'cache.headers:public;s_maxage=300,etag', // for the CDN's benefit: the WP user agent does not cache at all.
     ])
     ->group(function (Router $router) {
+        // @formatter:off
+
+        $router->any('/core/browse-happy/{version}', BrowseHappyController::class)->where(['version' => '1.1']);
+        $router->any('/core/serve-happy/{version}', ServeHappyController::class)->where(['version' => '1.0']);
+        $router->get('/core/importers/{version}', ImportersController::class)->where(['version' => '1.[01]']);
+
+        $router->get('/plugins/info/1.2', PluginInformation_1_2_Controller::class);
+        $router->post('/plugins/update-check/1.1', PluginUpdateCheck_1_1_Controller::class);
+
         $router->get('/secret-key/{version}', [SecretKeyController::class, 'index'])->where(['version' => '1.[01]']);
         $router->get('/secret-key/{version}/salt', [SecretKeyController::class, 'salt'])->where(['version' => '1.1']);
 
-        $router->any('/stats/wordpress/{version}', CatchAllController::class)->where(['version' => '1.0']);
-        $router->any('/stats/php/{version}', CatchAllController::class)->where(['version' => '1.0']);
-        $router->any('/stats/mysql/{version}', CatchAllController::class)->where(['version' => '1.0']);
-        $router->any('/stats/locale/{version}', CatchAllController::class)->where(['version' => '1.0']);
-        $router->any('/stats/plugin/{version}/downloads.php', CatchAllController::class)->where(['version' => '1.0']);
-        $router->any('/stats/plugin/{version}/{slug}', CatchAllController::class)->where(['version' => '1.0']);
+        $router->get('/themes/info/{version}', [ThemeController::class, 'info'])->where(['version' => '1.[012]']);
+        $router->match(['get', 'post'], '/themes/update-check/{version}', ThemeUpdatesController::class)->where(['version' => '1.[01]']);
 
-        $router->any('/core/browse-happy/{version}', BrowseHappyController::class)->where(['version' => '1.1']);
+        // CatchAll passes the request through to .org (and probably should be renamed)
+
         $router->any('/core/checksums/{version}', CatchAllController::class)->where(['version' => '1.0']);
         $router->any('/core/credits/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
         $router->any('/core/handbook/{version}', CatchAllController::class)->where(['version' => '1.0']);
-
-        $router->get('/core/importers/{version}', ImportersController::class)->where(['version' => '1.[01]']);
-
-        $router->any('/core/serve-happy/{version}', ServeHappyController::class)->where(['version' => '1.0']);
         $router->any('/core/stable-check/{version}', CatchAllController::class)->where(['version' => '1.0']);
         $router->any('/core/version-check/{version}', CatchAllController::class)->where(['version' => '1.[67]']);
+
+        $router->any('/events/{version}', CatchAllController::class)->where(['version' => '1.0']);
+
+        $router->any('/patterns/{version}', CatchAllController::class)->where(['version' => '1.0']);
+
+        // /plugins endpoints are implemented for version 1.2, older versions are still pass-through
+        $router->any('/plugins/info/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
+        $router->any('/plugins/update-check/{version}', CatchAllController::class)->where(['version' => '1.0']);
+
+        $router->any('/stats/locale/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $router->any('/stats/mysql/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $router->any('/stats/php/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        $router->any('/stats/plugin/{version}/downloads.php', CatchAllController::class)->where(['version' => '1.0']);
+        $router->any('/stats/plugin/{version}/{slug}', CatchAllController::class)->where(['version' => '1.0']);
+        $router->any('/stats/wordpress/{version}', CatchAllController::class)->where(['version' => '1.0']);
 
         $router->any('/translations/core/{version}', CatchAllController::class)->where(['version' => '1.0']);
         $router->any('/translations/plugins/{version}', CatchAllController::class)->where(['version' => '1.0']);
         $router->any('/translations/themes/{version}', CatchAllController::class)->where(['version' => '1.0']);
 
-        $router->get('/themes/info/{version}', [ThemeController::class, 'info'])->where(['version' => '1.[012]']);
-        $router->match(['get', 'post'], '/themes/update-check/{version}', ThemeUpdatesController::class)->where(
-            ['version' => '1.[01]'],
-        );
-
-        $router->get('/plugins/info/1.2', PluginInformation_1_2_Controller::class);
-        $router->any('/plugins/info/{version}', CatchAllController::class)->where(['version' => '1.[01]']);
-
-        $router->post('/plugins/update-check/1.1', PluginUpdateCheck_1_1_Controller::class);
-        $router->any('/plugins/update-check/{version}', CatchAllController::class)->where(['version' => '1.0']);
-
-        $router->any('/patterns/{version}', CatchAllController::class)->where(['version' => '1.0']);
-        $router->any('/events/{version}', CatchAllController::class)->where(['version' => '1.0']);
+        // @formatter:on
     });
 
 // Route::any('{path}', CatchAllController::class)->where('path', '.*');
