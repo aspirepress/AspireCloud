90a9f291246471a72bf414cb6d05e3429e4a49cb
Chuck Adams <chaz@chaz.works>
2025-03-31 11:35:17 -0600

    test: auto-fake facades

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,32 @@
 <?php
 
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Storage;
 
 abstract class TestCase extends BaseTestCase
 {
     protected bool $seed = true;
+
+    /** @var list<class-string<Facade>> */
+    protected array $fakeFacades = [
+        Http::class,
+        Mail::class,
+        Queue::class,
+        Storage::class,
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        foreach ($this->fakeFacades as $facade) {
+            $facade::fake();
+        }
+    }
 }
