21f08cb549b6fc2f686724bd1d43ba733b90ed05
Chuck Adams <cja987@gmail.com>
2025-04-14 17:49:32 -0600

    fix: use lower-level sql for hot_tags.  never use Eloquent's withCount() (#234)

--- app/Services/Themes/ThemeHotTagsService.php
+++ app/Services/Themes/ThemeHotTagsService.php
@@ -15,10 +15,20 @@ class ThemeHotTagsService
     public function getHotTags(int $count = -1): array
     {
         $hotTags = ThemeTag::query()
-            ->withCount('themes')
+            ->select('theme_tags.*')
+            ->join('theme_theme_tags', 'theme_tags.id', '=', 'theme_theme_tags.theme_tag_id')
+            ->selectRaw('COUNT(theme_theme_tags.theme_id) as themes_count')
+            ->groupBy('theme_tags.id', 'theme_tags.name', 'theme_tags.slug')
             ->orderBy('themes_count', 'desc')
             ->limit($count >= 0 ? $count : 100)
             ->get();
+
+        // this blows up the database.  do not use this.
+        // $hotTags = ThemeTag::query()
+        //     ->withCount('themes')
+        //     ->orderBy('themes_count', 'desc')
+        //     ->limit($count >= 0 ? $count : 100)
+        //     ->get();
 
         return ThemeHotTagsResponse::collect($hotTags)
             ->mapWithKeys(fn(ThemeHotTagsResponse $tag) => [$tag->slug => $tag])
