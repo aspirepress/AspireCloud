def03d38deb8c723e0c1f2c5258ec08f2ff4d2ce
Chuck Adams <chaz@chaz.works>
2025-04-14 17:27:30 -0600

    fix: use lower-level sql for hot_tags.  never use Eloquent's withCount()

--- app/Services/Plugins/PluginHotTagsService.php
+++ app/Services/Plugins/PluginHotTagsService.php
@@ -15,10 +15,20 @@ class PluginHotTagsService
     public function getHotTags(int $count = -1): array
     {
         $hotTags = PluginTag::query()
-            ->withCount('plugins')
+            ->select('plugin_tags.*')
+            ->join('plugin_plugin_tags', 'plugin_tags.id', '=', 'plugin_plugin_tags.plugin_tag_id')
+            ->selectRaw('COUNT(plugin_plugin_tags.plugin_id) as plugins_count')
+            ->groupBy('plugin_tags.id', 'plugin_tags.name', 'plugin_tags.slug')
             ->orderBy('plugins_count', 'desc')
             ->limit($count >= 0 ? $count : 100)
             ->get();
+
+        // This was the first attempt, and it blows up the database with nested scans.  Do not do this.
+        // $hotTags = PluginTag::query()
+        //     ->withCount('plugins')
+        //     ->orderBy('plugins_count', 'desc')
+        //     ->limit($count >= 0 ? $count : 100)
+        //     ->get();
 
         return PluginHotTagsResponse::collect($hotTags)
             ->mapWithKeys(fn(PluginHotTagsResponse $tag) => [$tag->slug => $tag])
