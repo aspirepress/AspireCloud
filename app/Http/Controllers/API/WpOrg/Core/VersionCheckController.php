dff31d94e81c94cce7d1c0fb47f3e83d608692fd
Chuck Adams <chaz@chaz.works>
2025-06-17 16:20:07 -0600

    reformat

--- app/Http/Controllers/API/WpOrg/Core/VersionCheckController.php
+++ app/Http/Controllers/API/WpOrg/Core/VersionCheckController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\API\WpOrg\Core;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class VersionCheckController extends Controller
 {
@@ -24,7 +24,7 @@ class VersionCheckController extends Controller
 
     public function __invoke(Request $request): JsonResponse
     {
-        $this->currentVersion = $request->query('version') ?? (string) PHP_INT_MAX;
+        $this->currentVersion = $request->query('version') ?? (string)PHP_INT_MAX;
         $this->locale = $request->query('locale') ?? '';
 
         $response = new \stdClass();
@@ -70,10 +70,20 @@ class VersionCheckController extends Controller
     private function buildTranslatedUpgradeOffer(string $version): \stdClass
     {
         $offer = $this->buildUpgradeOffer($version);
-        $offer->download = config('app.aspirecloud.download.base') . 'release/' . $this->locale . '/wordpress-' . $version . '.zip';
+        $offer->download = config('app.aspirecloud.download.base')
+            . 'release/'
+            . $this->locale
+            . '/wordpress-'
+            . $version
+            . '.zip';
         $offer->locale = $this->locale;
 
-        $offer->packages->full = config('app.aspirecloud.download.base') . 'release/' . $this->locale . '/wordpress-' . $version . '.zip';
+        $offer->packages->full = config('app.aspirecloud.download.base')
+            . 'release/'
+            . $this->locale
+            . '/wordpress-'
+            . $version
+            . '.zip';
         $offer->packages->no_content = false;
         $offer->packages->new_bundled = false;
         $offer->packages->partial = false;
@@ -98,12 +108,18 @@ class VersionCheckController extends Controller
 
         $offer->response = $this->currentVersion === $version ? 'latest' : 'upgrade';
         $offer->download = config('app.aspirecloud.download.base') . 'release/wordpress-' . $version . '.zip';
-        $offer->locale = $offer->response === 'latest' && ! $this->locale ? false : 'en_US';
+        $offer->locale = $offer->response === 'latest' && !$this->locale ? false : 'en_US';
 
         $offer->packages = new \stdClass();
         $offer->packages->full = config('app.aspirecloud.download.base') . 'release/wordpress-' . $version . '.zip';
-        $offer->packages->no_content = config('app.aspirecloud.download.base') . 'release/wordpress-' . $version . '-no-content.zip';
-        $offer->packages->new_bundled = config('app.aspirecloud.download.base') . 'release/wordpress-' . $version . '-new-bundled.zip';
+        $offer->packages->no_content = config('app.aspirecloud.download.base')
+            . 'release/wordpress-'
+            . $version
+            . '-no-content.zip';
+        $offer->packages->new_bundled = config('app.aspirecloud.download.base')
+            . 'release/wordpress-'
+            . $version
+            . '-new-bundled.zip';
 
         // Disabling these for now, pending more research from the team.
         $offer->packages->partial = false;
@@ -124,7 +140,7 @@ class VersionCheckController extends Controller
      */
     private function buildTranslations(): array
     {
-        if (! $this->locale) {
+        if (!$this->locale) {
             return [];
         }
 
@@ -136,10 +152,15 @@ class VersionCheckController extends Controller
         $translation->language = $this->locale;
         $translation->version = $this->currentVersion;
         $translation->updated = '2023-10-01T00:00:00Z'; // Store in DB from the translations file. Pull from DB for here.
-        $translation->package = config('app.aspirecloud.download.base') . 'translation/core/' . $this->currentVersion . '/' . $this->locale . '.zip';
+        $translation->package = config('app.aspirecloud.download.base')
+            . 'translation/core/'
+            . $this->currentVersion
+            . '/'
+            . $this->locale
+            . '.zip';
         $translation->autoupdate = true;
 
-        return [ $translation ];
+        return [$translation];
     }
 
     private function getLatestVersion(): string
@@ -164,17 +185,63 @@ class VersionCheckController extends Controller
         // If not going for completeness, can probably remove the versions
         // below AspireUpdate's minimum WP version (currently 5.3).
         $map = [
-            '6.8.1', '6.8', '6.7.2', '6.6.2', '6.6.5', '6.4.5', '6.3.5', '6.2.6', '6.1.7', '6.0.9',
-            '5.9.10', '5.8.10', '5.7.12', '5.6.14', '5.5.15', '5.4.16', '5.3.18', '5.2.21', '5.1.19', '5.0.22',
-            '4.9.26', '4.8.25', '4.7.29', '4.6.29', '4.5.32', '4.4.33', '4.3.34', '4.2.38', '4.1.41', '4.0.38',
-            '3.9.40', '3.8.41', '3.7.41', '3.6.1', '3.5.2', '3.4.2', '3.3.3', '3.2.1', '3.1.4', '3.0.6',
-            '2.9.2', '2.8.6', '2.7.1', '2.6.5', '2.5.1', '2.3.3', '2.2.3', '2.1.3', '2.0.11',
-            '1.5.2', '1.2.2', '1.0.2',
+            '6.8.1',
+            '6.8',
+            '6.7.2',
+            '6.6.2',
+            '6.6.5',
+            '6.4.5',
+            '6.3.5',
+            '6.2.6',
+            '6.1.7',
+            '6.0.9',
+            '5.9.10',
+            '5.8.10',
+            '5.7.12',
+            '5.6.14',
+            '5.5.15',
+            '5.4.16',
+            '5.3.18',
+            '5.2.21',
+            '5.1.19',
+            '5.0.22',
+            '4.9.26',
+            '4.8.25',
+            '4.7.29',
+            '4.6.29',
+            '4.5.32',
+            '4.4.33',
+            '4.3.34',
+            '4.2.38',
+            '4.1.41',
+            '4.0.38',
+            '3.9.40',
+            '3.8.41',
+            '3.7.41',
+            '3.6.1',
+            '3.5.2',
+            '3.4.2',
+            '3.3.3',
+            '3.2.1',
+            '3.1.4',
+            '3.0.6',
+            '2.9.2',
+            '2.8.6',
+            '2.7.1',
+            '2.6.5',
+            '2.5.1',
+            '2.3.3',
+            '2.2.3',
+            '2.1.3',
+            '2.0.11',
+            '1.5.2',
+            '1.2.2',
+            '1.0.2',
             '0.72',
         ];
 
         // If no version has been provided, all versions are considered older.
-        if ($this->currentVersion === (string) PHP_INT_MAX) {
+        if ($this->currentVersion === (string)PHP_INT_MAX) {
             return $map;
         }
 
