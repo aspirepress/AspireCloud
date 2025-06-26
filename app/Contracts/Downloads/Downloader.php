bbb7f2142d8ebed537972325a7fd7e3a749aec67
Chuck Adams <chaz@chaz.works>
2025-06-25 21:13:10 -0600

    pass through to s3 if Cdn-Loop header is present

--- app/Contracts/Downloads/Downloader.php
+++ app/Contracts/Downloads/Downloader.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 namespace App\Contracts\Downloads;
 
 use App\Enums\AssetType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 interface Downloader
 {
-    public function download(AssetType $type, string $slug, string $file, ?string $revision = null): Response;
+    public function download(
+        Request $request,
+        AssetType $type,
+        string $slug,
+        string $file,
+        ?string $revision = null,
+    ): Response;
 }
--- app/Http/Controllers/API/WpOrg/Downloads/DownloadThemeController.php
+++ app/Http/Controllers/API/WpOrg/Downloads/DownloadThemeController.php
@@ -6,6 +6,7 @@ use App\Enums\AssetType;
 use App\Http\Controllers\Controller;
 use App\Services\Downloads\DownloadService;
 use App\Utils\Regex;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DownloadThemeController extends Controller
@@ -14,13 +15,13 @@ class DownloadThemeController extends Controller
         private readonly DownloadService $downloadService,
     ) {}
 
-    public function __invoke(string $file): Response
+    public function __invoke(Request $request, string $file): Response
     {
         $matches = Regex::match('/^([a-zA-Z0-9-]+)\.(.+)\.zip$/', $file);
         if (!$matches) {
             return response()->json(['error' => "Invalid filename", 'filename' => $file], 400);
         }
 
-        return $this->downloadService->download(type: AssetType::THEME, slug: $matches[1], file: $file);
+        return $this->downloadService->download($request, type: AssetType::THEME, slug: $matches[1], file: $file);
     }
 }
