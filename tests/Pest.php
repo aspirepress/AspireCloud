7c665cd511cfaf2bb3007659ae736fdbf4343d6d
Chuck Adams <chaz@chaz.works>
2025-07-28 10:32:33 -0600

    fake storage for all tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 pest()
     ->extend(TestCase::class)
     ->use(RefreshDatabase::class)
     ->beforeEach(function () {
+        Storage::fake();
         Http::preventStrayRequests();
         $this->withoutVite();
     })
