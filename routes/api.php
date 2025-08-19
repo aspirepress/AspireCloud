70627cff51d6a1f63f22b32a000705849fe941bd
Najm Njeim <nnjeim@crowdfavorite.com>
2025-08-19 14:52:46 +0300

    reversal of the api routes rework

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\API\WpOrg\Core\BrowseHappyController;
 use App\Http\Controllers\API\WpOrg\Core\ImportersController;
 use App\Http\Controllers\API\WpOrg\Core\ServeHappyController;
 use App\Http\Controllers\API\WpOrg\Core\StableCheckController;
+use App\Http\Controllers\API\WpOrg\Export\ExportController;
 use App\Http\Controllers\API\WpOrg\Plugins\PluginInformation_1_0_Controller;
 use App\Http\Controllers\API\WpOrg\Plugins\PluginInformation_1_2_Controller;
 use App\Http\Controllers\API\WpOrg\Plugins\PluginUpdateCheck_1_1_Controller;
@@ -26,77 +27,56 @@ Route::prefix('/')
         'cache.headers:public;s_maxage=300,etag', // for the CDN's benefit: the WP user agent does not cache at all.
     ])
     ->group(function (Router $router) {
-        // core routes
-        $router->group([
-            'prefix' => 'core',
-        ], function (Router $router) {
-            // @formatter:off
-            $router->any('/browse-happy/{version}', BrowseHappyController::class)->where(['version' => '1.1']);
-            $router->any('/serve-happy/{version}', ServeHappyController::class)->where(['version' => '1.0']);
-            $router->match(['get', 'post'], '/stable-check/{version}', StableCheckController::class)->where(['version' => '1.0']);
-            $router->get('/importers/{version}', ImportersController::class)->where(['version' => '1.[01]']);
-            /// Pass-through routes still going to .org
-            $router->any('/checksums/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/credits/{version}', PassThroughController::class)->where(['version' => '1.[01]']);
-            $router->any('/handbook/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/version-check/{version}', PassThroughController::class)->where(['version' => '1.[67]']);
-        });
-        // plugins routes
-        $router->group([
-            'prefix' => 'plugins',
-        ], function (Router $router) {
-            // Plugin information routes
-            $router->get('/info/1.0/{slug}.json', PluginInformation_1_0_Controller::class);
-            $router->get('/info/1.2', PluginInformation_1_2_Controller::class);
-            // Plugins endpoints are implemented for version 1.2, older versions are still pass-through
-            $router->any('/info/{version}', PassThroughController::class)->where(['version' => '1.[01]']);
+        // @formatter:off
 
-           // Plugin update check routes
-            $router->post('/update-check/1.1', PluginUpdateCheck_1_1_Controller::class);
-            // Plugins endpoints are implemented for version 1.2, older versions are still pass-through
-            $router->any('/update-check/{version}', PassThroughController::class)->where(['version' => '1.0']);
-        });
-        // secret key routes
-        $router->group([
-            'prefix' => 'secret-key',
-        ], function (Router $router) {
-            $router->get('/{version}', [SecretKeyController::class, 'index'])->where(['version' => '1.[01]']);
-            $router->get('/{version}/salt', [SecretKeyController::class, 'salt'])->where(['version' => '1.1']);
-        });
-        // themes routes
-        $router->group([
-            'prefix' => 'themes',
-        ], function (Router $router) {
-            $router->get('/info/{version}', [ThemeController::class, 'info'])->where(['version' => '1.[012]']);
-            $router->match(['get', 'post'], '/update-check/{version}', ThemeUpdatesController::class)->where(['version' => '1.[01]']);
-        });
-        // events routes
+        $router->any('/core/browse-happy/{version}', BrowseHappyController::class)->where(['version' => '1.1']);
+        $router->any('/core/serve-happy/{version}', ServeHappyController::class)->where(['version' => '1.0']);
+        $router->match(['get', 'post'], '/core/stable-check/{version}', StableCheckController::class)->where(['version' => '1.0']);
+        $router->get('/core/importers/{version}', ImportersController::class)->where(['version' => '1.[01]']);
+
+        $router->get('/export/{type}', ExportController::class)
+            ->whereIn('type', ['plugins', 'themes', 'closed_plugins']);
+
+        $router->get('/plugins/info/1.0/{slug}.json', PluginInformation_1_0_Controller::class);
+        $router->get('/plugins/info/1.2', PluginInformation_1_2_Controller::class);
+        $router->post('/plugins/update-check/1.1', PluginUpdateCheck_1_1_Controller::class);
+
+        $router->get('/secret-key/{version}', [SecretKeyController::class, 'index'])->where(['version' => '1.[01]']);
+        $router->get('/secret-key/{version}/salt', [SecretKeyController::class, 'salt'])->where(['version' => '1.1']);
+
+        $router->get('/themes/info/{version}', [ThemeController::class, 'info'])->where(['version' => '1.[012]']);
+        $router->match(['get', 'post'], '/themes/update-check/{version}', ThemeUpdatesController::class)->where(['version' => '1.[01]']);
+
+        /// Pass-through routes still going to .org
+
+        $router->any('/core/checksums/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/core/credits/{version}', PassThroughController::class)->where(['version' => '1.[01]']);
+        $router->any('/core/handbook/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/core/version-check/{version}', PassThroughController::class)->where(['version' => '1.[67]']);
+
         $router->any('/events/{version}', PassThroughController::class)->where(['version' => '1.0']);
-        // pattern routes
+
         $router->any('/patterns/{version}', PassThroughController::class)->where(['version' => '1.0']);
-        // stats routes
-        $router->group([
-            'prefix' => 'stats',
-        ], function (Router $router) {
-            $router->any('/locale/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/mysql/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/php/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/plugin/{version}/downloads.php', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/plugin/{version}/{slug}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/wordpress/{version}', PassThroughController::class)->where(['version' => '1.0']);
-        });
-        // translations routes
-        $router->group([
-            'prefix' => 'translations',
-        ], function (Router $router) {
-            $router->any('/core/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/plugins/{version}', PassThroughController::class)->where(['version' => '1.0']);
-            $router->any('/themes/{version}', PassThroughController::class)->where(['version' => '1.0']);
-        });
+
+        // /plugins endpoints are implemented for version 1.2, older versions are still pass-through
+        $router->any('/plugins/info/{version}', PassThroughController::class)->where(['version' => '1.[01]']);
+        $router->any('/plugins/update-check/{version}', PassThroughController::class)->where(['version' => '1.0']);
+
+        $router->any('/stats/locale/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/stats/mysql/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/stats/php/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/stats/plugin/{version}/downloads.php', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/stats/plugin/{version}/{slug}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/stats/wordpress/{version}', PassThroughController::class)->where(['version' => '1.0']);
+
+        $router->any('/translations/core/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/translations/plugins/{version}', PassThroughController::class)->where(['version' => '1.0']);
+        $router->any('/translations/themes/{version}', PassThroughController::class)->where(['version' => '1.0']);
+
         // @formatter:on
     });
 
 // Route::any('{path}', CatchAllController::class)->where('path', '.*');
 
-    require __DIR__ . '/inc/admin-api.php';
-    require __DIR__ . '/inc/download.php';
+require __DIR__ . '/inc/admin-api.php';
+require __DIR__ . '/inc/download.php';
