124865326cf9ab1feebe61fb599af338985afb07
Chuck Adams <chaz@chaz.works>
2025-08-21 08:58:22 -0600

    port new applySearchWeighted from PR #314

--- app/Services/Plugins/QueryPluginsService.php
+++ app/Services/Plugins/QueryPluginsService.php
@@ -1,12 +1,10 @@
 <?php
 
-namespace App\Services\Plugins;
+namespace App\Services\PluginServices;
 
 use App\Models\WpOrg\Plugin;
 use App\Utils\Regex;
-use App\Values\WpOrg\Plugins\PluginResponse;
 use App\Values\WpOrg\Plugins\QueryPluginsRequest;
-use App\Values\WpOrg\Plugins\QueryPluginsResponse;
 use Illuminate\Database\Eloquent\Builder;
 
 class QueryPluginsService
@@ -16,18 +14,14 @@ class QueryPluginsService
         $page = $req->page;
         $perPage = $req->per_page;
         $browse = $req->browse ?: 'popular';
-        $search = $req->search;
-        $tag = $req->tags[0] ?? null;   // TODO: multiple tags support
-        $author = $req->author;
-
-        $search = self::normalizeSearchString($search);
-        $tag = self::normalizeSearchString($tag);
-        $author = self::normalizeSearchString($author);
+        $search = $req->search ?? null;
+        $tags = $req->tags ?? [];
+        $author = $req->author ?? null;
 
         // Ad hoc pipeline because Laravel's Pipeline class is awful
         $callbacks = collect();
         $search and $callbacks->push(fn($query) => self::applySearchWeighted($query, $search, $req));
-        $tag and $callbacks->push(fn($query) => self::applyTag($query, $tag));
+        $tags and $callbacks->push(fn($query) => self::applyTag($query, $tags));
         $author and $callbacks->push(fn($query) => self::applyAuthor($query, $author));
         !$search and $callbacks->push(fn($query) => self::applyBrowse($query, $browse)); // search applies its own sort
 
@@ -43,9 +37,9 @@ class QueryPluginsService
             ->limit($perPage)
             ->get()
             ->unique('slug')
-            ->map(fn($plugin) => PluginResponse::from($plugin));
+            ->map(fn($plugin) => Plugins\PluginResponse::from($plugin));
 
-        return QueryPluginsResponse::from([
+        return Plugins\QueryPluginsResponse::from([
             'plugins' => $plugins,
             'info' => ['page' => $page, 'pages' => $totalPages, 'results' => $total],
         ]);
@@ -57,80 +51,66 @@ class QueryPluginsService
      * @param Builder<Plugin> $query
      * @return Builder<Plugin> Returns a new query with weighted search applied
      */
-    public static function applySearchWeighted(Builder $query, string $search, QueryPluginsRequest $request): Builder
-    {
+    public static function applySearchWeighted(
+        Builder $query,
+        string $search,
+        Plugins\QueryPluginsDTO $request,
+    ): Builder {
         $lcsearch = mb_strtolower($search);
         $slug = Regex::replace('/[^-\w]+/', '-', $lcsearch);
         $wordchars = Regex::replace('/\W+/', '', $lcsearch);
         $sortColumn = self::browseToSortColumn($request->browse);
 
-        $weightSimilar = fn($column) => "pow(5 * similarity($column, '$wordchars'), 2) * log(greatest($sortColumn, 1))";
-
-        $slugExact = Plugin::query()
-            ->where('slug', $search)
-            ->selectRaw("*, 999999999999 as score");
-
-        $nameExact = Plugin::query()
-            ->where('name', $search)
-            ->selectRaw("*, 999999999999 as score");
-
-        $slugPrefix = Plugin::query()
-            ->whereLike('slug', "$slug%")
-            ->selectRaw("*, 2 * log(greatest($sortColumn, 1)) as score");
-
-        $namePrefix = Plugin::query()
-            ->whereLike('name', "$search%")
-            ->selectRaw("*, 2 * log(greatest($sortColumn, 1)) as score");
-
-        $slugSimilar = Plugin::query()
-            ->whereRaw("slug %> '$wordchars'")
-            ->selectRaw("*, " . $weightSimilar('slug') . " as score");
-
-        $nameSimilar = Plugin::query()
-            ->whereRaw("name %> '$wordchars'")
-            ->selectRaw("*, " . $weightSimilar('name') . " as score");
-
-        $shortDescSimilar = Plugin::query()
-            ->whereRaw("short_description %> '$wordchars'")
-            ->selectRaw("*, " . $weightSimilar('short_description') . " as score");
-
-        $descFulltext = Plugin::query()
-            ->whereFullText('description', $search)
-            ->selectRaw("*, 3 * log(greatest($sortColumn, 1)) as score");
-
-        $baseQuery = $slugExact;
-        $baseQuery->unionAll($nameExact);
-        $baseQuery->unionAll($slugPrefix);
-        $baseQuery->unionAll($namePrefix);
-        $baseQuery->unionAll($slugSimilar);
-        $baseQuery->unionAll($nameSimilar);
-        $baseQuery->unionAll($shortDescSimilar);
-        $baseQuery->unionAll($descFulltext);
-
-        return Plugin::query()
-            ->fromSub($baseQuery, 'weighted_plugins')
-            ->orderBy('score', 'desc');
+        return $query
+            ->where(fn($q) => $q
+                ->where('slug', $search)
+                ->orWhere('name', 'like', "$search%")
+                ->orWhereRaw("slug %> ?", [$wordchars])
+                ->orWhereRaw("name %> ?", [$wordchars])
+                ->orWhereRaw("short_description %> ?", [$wordchars])
+                ->orWhereFullText('description', $search),
+            )
+            ->selectRaw("plugins.*,
+            CASE
+                WHEN slug = ? THEN 1000000
+                WHEN name = ? THEN 900000
+                WHEN slug LIKE ? THEN 800000
+                WHEN name LIKE ? THEN 700000
+                WHEN slug %> ? THEN 600000
+                WHEN name %> ? THEN 500000
+                WHEN short_description %> ? THEN 400000
+                WHEN to_tsvector('english', description) @@ plainto_tsquery(?) THEN 300000
+                ELSE 0
+            END + log(GREATEST($sortColumn, 1)) AS score", [
+                $search,
+                $search,
+                "$slug%",
+                "$search%",
+                $wordchars,
+                $wordchars,
+                $wordchars,
+                $search,
+            ])
+            ->orderByDesc('score');
     }
 
     /** @param Builder<Plugin> $query */
     public static function applyAuthor(Builder $query, string $author): Builder
     {
-        return $query->where(fn(Builder $q)
-            => $q
+        return $query->where(fn(Builder $q) => $q
             ->whereRaw("author %> '$author'")
             ->orWhereHas(
                 'contributors',
-                fn(Builder $q)
-                    => $q
+                fn(Builder $q) => $q
                     ->whereRaw("user_nicename %> '$author'")
                     ->orWhereRaw("display_name %> '$author'"),
             ));
     }
 
     /** @param Builder<Plugin> $query */
-    public static function applyTag(Builder $query, string $tag): Builder
+    public static function applyTag(Builder $query, array $tags): Builder
     {
-        return $query->whereHas('tags', fn(Builder $q) => $q->whereIn('slug', [$tag]));
+        return $query->whereHas('tags', fn(Builder $q) => $q->whereIn('slug', $tags));
     }
 
     /**
@@ -151,15 +131,5 @@ class QueryPluginsService
             'top-rated', 'featured' => 'rating',
             default => 'active_installs',
         };
-    }
-
-    public static function normalizeSearchString(?string $search): ?string
-    {
-        if ($search === null) {
-            return null;
-        }
-        $search = trim($search);
-        $search = Regex::replace('/\s+/i', ' ', $search);
-        return Regex::replace('/[^\w.,!?@#$_-]/i', ' ', $search); // strip most punctuation, allow a small subset
     }
 }
