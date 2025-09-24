d2e900649aafdb12cf1952fa942038206da9de6e
Najm Njeim <nnjeim@crowdfavorite.com>
2025-09-24 14:12:40 +0300

    Modification of the elasticsearch search method to allow pagination

--- app/Http/Controllers/API/Elastic/ElasticSearchController.php
+++ app/Http/Controllers/API/Elastic/ElasticSearchController.php
@@ -2,36 +2,65 @@
 
 namespace App\Http\Controllers\API\Elastic;
 
-use Illuminate\Http\Request;
-use Elastic\Elasticsearch\Client;
 use App\Http\Controllers\Controller;
+use Elastic\Elasticsearch\Client;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class ElasticSearchController extends Controller
 {
-    public function search(Request $request, Client $client)
+    /**
+     * Search plugins
+     *
+     * @param Request $request
+     * @param Client $client
+     * @return JsonResponse
+     */
+    public function search(Request $request, Client $client): JsonResponse
     {
-        $query = $request->input('q', '');
-
-        if (empty($query)) {
-            return response()->json(['error' => 'Missing search query'], 400);
-        }
-
-        $response = $client->search([
-            'index' => 'plugins',
-            'body'  => [
+        $query = trim($request->input('q', ''));
+        $limit = (int)$request->input('limit', 1000);
+        $offset = (int)$request->input('offset', 0);
+        // query
+        if ($query === '') {
+            $body = [
+                'query' => [
+                    'match_all' => (object)[],
+                ],
+            ];
+        } else {
+            $body = [
                 'query' => [
                     'bool' => [
                         'should' => [
-                            ['wildcard' => ['name'        => '*' . strtolower($query) . '*']],
-                            ['wildcard' => ['slug'        => '*' . strtolower($query) . '*']],
+                            ['wildcard' => ['name' => '*' . strtolower($query) . '*']],
+                            ['wildcard' => ['slug' => '*' . strtolower($query) . '*']],
                             ['wildcard' => ['description' => '*' . strtolower($query) . '*']],
                         ],
                         'minimum_should_match' => 1,
                     ],
                 ],
-            ],
-        ]);
+            ];
+        }
+        // search
+        $response = $client
+            ->search([
+                'index' => 'plugins',
+                'body' => $body,
+                'from' => $offset,
+                'size' => $limit,
+            ]);
 
-        return response()->json($response->asArray());
+        $hits = $response['hits']['hits'] ?? [];
+        $total = $response['hits']['total']['value'] ?? 0;
+        $results = array_map(fn($hit) => $hit['_source'], $hits);
+        // response
+        return response()
+            ->json([
+                'total' => $total,
+                'limit' => $limit,
+                'offset' => $offset,
+                'results' => $results,
+            ]);
     }
 }
