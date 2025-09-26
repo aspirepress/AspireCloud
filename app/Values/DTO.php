f5bbaa5b0a45a0238fefd35de7edf6060b406ad7
Chuck Adams <chaz@chaz.works>
2025-09-26 13:56:38 -0600

    enable invalid-parent-type (inline ToJsonPretty trait)

--- app/Values/DTO.php
+++ app/Values/DTO.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 namespace App\Values;
 
 use Bag\Bag;
+use Override;
 
 abstract readonly class DTO extends Bag
 {
-    use ToJsonPretty;
+    public const DEFAULT_JSON_FLAGS =
+        JSON_THROW_ON_ERROR
+        | JSON_PRETTY_PRINT
+        | JSON_UNESCAPED_SLASHES
+        | JSON_UNESCAPED_UNICODE;
+
+    #[Override]
+    public function toJson($options = 0): string|false
+    {
+        return parent::toJson($options | static::DEFAULT_JSON_FLAGS);
+    }
 }
