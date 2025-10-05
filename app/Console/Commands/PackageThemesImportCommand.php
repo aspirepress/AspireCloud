8d4fd9c22656fcaba58f745bb3142db9aac8631d
Chuck Adams <chaz@chaz.works>
2025-10-05 13:47:12 -0600

    prevent runaway transaction nesting in package import commands

--- app/Console/Commands/PackageThemesImportCommand.php
+++ app/Console/Commands/PackageThemesImportCommand.php
@@ -45,6 +45,7 @@ class PackageThemesImportCommand extends Command
                         ])
                         ->first();
                     if ($package && $this->option('new-only')) {
+                        DB::rollBack();
                         return;
                     }
                     $package?->delete();
