03e098c58e440e442890a4f1acd05f1cdeb48c15
Chuck Adams <chaz@chaz.works>
2025-10-26 09:54:32 -0600

    remove laravel/telescope.  i gave it plenty of chances.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,5 +6,4 @@ return [
     App\Providers\ElasticsearchServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
 ];
